1272df9b4f9505ec08112aa6a81ebb562dfaffaf
MaximHelio <gasiorowicz10@gmail.com>
2024-04-12 21:29:37 +0900

    Product detail

---
+++ app/vancleef/detail/[product_pk]/page.tsx
@@ -0,0 +1,13 @@
+import ProductsDetailInfo from "@/components/products/products-detail-info"
+import ProductsDetailContent from "@/components/products/products-detail-content"
+
+const Detail = () => {
+  return (
+    <div>
+      <ProductsDetailInfo />
+      <ProductsDetailContent />
+    </div>
+  )
+}
+
+export default Detail
